6fbdf92656881b78da8b394276eb2301849491c7
Andy Kauffman <schlefty@gmail.com>
2018-04-19 08:07:35 -0400

    jira auth and web changes
    Update Jira Authentication to be based on system parameter.
    Switch to WEB project for new scrs

--- src/server/jiraApi.js
+++ src/server/jiraApi.js
@@ -2,62 +2,81 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 
-//Auth token for Andy's account
-const ANDY_AUTH = 'Basic QW5keS5LYXVmZm1hbjpTY2hsZWZ0eTE=';
+//Auth token for Andy's account base64 encoded from username:password
+let credentials = process.env.JIRA_USER + ':' + process.env.JIRA_PASSWORD;
+const JIRA_AUTH = 'Basic ' + new Buffer(credentials).toString('base64');
+console.info(JIRA_AUTH);
 
 const QUERY_URL = 'https://devjira.inin.com/rest/api/2/search?jql=%22Development%20Labels%22%20in%20(cart_issue)%20and%20%22Development%20Labels%22%20in%20(consistent_failure)%20and%20status%20!=%20resolved&fields=id,key,summary,customfield_10073';
 const ISSUE_URL = 'https://devjira.inin.com/rest/api/2/issue';
 
 let config = {
-    headers: {Authorization: ANDY_AUTH}
+    headers: { Authorization: JIRA_AUTH }
 };
 
 /* GET api listing. */
 router.get('/', (req, res) => {
-  res.send('jira api root');
+    res.send('jira api root');
 });
 
 // Get open issues from the query
 router.get('/issues', (req, res) => {
-  axios.get(QUERY_URL)
-    .then(result => {
-      res.status(200).json(result.data);
-    })
-    .catch(error => {
-      res.status(500).send(error)
-    });
+    console.info('Getting all issues');
+    axios.get(QUERY_URL)
+        .then(result => {
+            res.status(200).json(result.data);
+        })
+        .catch(error => {
+            res.status(500).send(error)
+        });
 });
 
 router.get('/issue/:issueKey', (req, res) => {
     let issueKey = req.params.issueKey;
+    console.info('Getting issue: ', issueKey);
 
     axios.get(ISSUE_URL + '/' + issueKey, config)
-      .then(result => {
-        res.status(200).json(result.data);
-      })
-      .catch(error => {
-        res.status(500).send(error)
-      });
-  });
+        .then(result => {
+            console.log(issueKey, ' retrieved successfully');
+            res.status(200).json(result.data);
+        })
+        .catch(error => {
+            console.log('Failed to retrieve issue: ', issueKey);
+            console.log('Error: ', error);
+            res.status(500).send('Error retrieving issue: ', issueKey);
+        });
+});
 
 // Create a new issue
 router.post('/issue', (req, res) => {
+    console.info('Creating new issue');
+
     let description = req.query.description;
     let teamLabel = req.query.teamLabel;
     let testCase = req.query.testCase;
 
     let newIssueKey = '';
     axios.post(ISSUE_URL, createIssueObj(description, teamLabel), config)
-      .then(result => {
+        .then(result => {
             newIssueKey = result.data.key;
             console.log('Issue ' + newIssueKey + ' created successfully');
             return axios.put(ISSUE_URL + '/' + newIssueKey, createEditObj(testCase), config);
         }).then(result => {
             console.log('Issue ' + newIssueKey + ' edited successfully');
-            res.status(200).json({issueKey: newIssueKey})
+            res.status(200).json({ issueKey: newIssueKey })
         }).catch(error => {
-            res.status(500).send(error)
-      });
+            if(error.response.status === 403) {
+                console.log('Authentication failed creating Jira issue.');
+                //NOTE: If authentication fails too many times, Jira may start responding with 'X-Authentication-Denied-Reason: CAPTCHA_CHALLENGE;'
+                // If that happens you will need to logout of a jira, log back in and answer the CAPTCHA before request will start being allowed.
+            } else {
+                console.log('Creating issue failed with response status: ', error.response.status);
+            }
+
+            console.log('Error: ', error);
+            
+            res.status(500).send('Error creating Jira issue.')
+        });
 });
 
 function createEditObj(testCase) {
@@ -73,9 +92,9 @@ function createIssueObj(description, teamLabel) {
     return {
         fields: {
             project:
-            {
-                key: "WICCLIENT"
-            },
+                {
+                    key: "WEB"
+                },
             description,
             issuetype: {
                 name: "Bug"
