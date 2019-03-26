1519b67e077c255d743a82976e50ac7ee316065d
Zak Eckert <zak.eckert@inin.com>
2019-03-26 11:07:49 -0400

    Further cleaning of jenkinsApi

--- src/server/jenkinsApi.js
+++ src/server/jenkinsApi.js
@@ -2,25 +2,29 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 
-const WEBIC_ICAT_URL = 'http://ci.qfun.com:8080/job/pureconnect/job/interaction_connect/job/webic-icat/'
-const MAIN_BASE_URL = 'http://ci.qfun.com:8080/job/pureconnect/job/interaction_connect/job/webic-icat/job/client.test.latest_systest/';
+const JENKINS_BASE_URL = 'http://ci.qfun.com:8080/';
+const CONNECT_JOB = 'job/pureconnect/job/interaction_connect/';
+const ICAT_JOB = 'job/webic-icat/';
+const MAIN_JOB = 'job/client.test.latest_systest/';
 const JSON_API = 'api/json';
 const LAST_SUCCESSFUL_ROUTE = 'lastSuccessfulBuild/testReport/';
 
+const WEBIC_ICAT_URL = JENKINS_BASE_URL + CONNECT_JOB + ICAT_JOB;
+const WEBIC_ICAT_URL_API = WEBIC_ICAT_URL + JSON_API;
+
+const MAIN_BASE_URL = WEBIC_ICAT_URL + MAIN_JOB;
 const MAIN_JOB_URL_API = MAIN_BASE_URL + JSON_API;
 const MAIN_LAST_SUCCESSFUL_TEST_REPORT_URL = MAIN_BASE_URL + LAST_SUCCESSFUL_ROUTE;
 const MAIN_LAST_SUCCESSFUL_TEST_REPORT_URL_API = MAIN_BASE_URL + LAST_SUCCESSFUL_ROUTE + JSON_API;
 
-const WEBIC_ICAT_URL_API = WEBIC_ICAT_URL + JSON_API;
-
-let getQAJobUrl = () => {
+function getQAJobUrl() {
     let qaRegex = new RegExp('client\.test\.([0-9]*)r([0-9])_systest');
     let qaJob;
     let currentYear = 0;
     let currentRelease = 0;
-    return axios.get(WEBIC_ICAT_URL_API).then((result) => {
+    return axios.get(WEBIC_ICAT_URL_API).then(result => {
         return result.data.jobs;
-    }).then((jobList) => {
+    }).then(jobList => {
         jobList.forEach((job) => {
             let result = job.name.match(qaRegex);
             if (result && result[1] >= currentYear && result[2] >= currentRelease) {
@@ -92,7 +96,7 @@ router.get('/main/test_report/:buildNumber', (req, res, next) => {
 
 // Get build information from the qa jenkins job
 router.get('/qa/job', (req, res, next) => {
-    getQAJobUrl().then((urlObj) => {
+    getQAJobUrl().then(urlObj => {
         let url = urlObj.qaJobUrl;
         console.log('Getting ' + url);
         return url;
@@ -107,7 +111,7 @@ router.get('/qa/job', (req, res, next) => {
 
 // Get build information for a specific build number
 router.get('/qa/build/:number', (req, res, next) => {
-    getQAJobUrl().then((urlObj) => {
+    getQAJobUrl().then(urlObj => {
         let url = urlObj.qaBaseUrl + req.params.number + '/' + JSON_API;
         console.log('Getting ' + url);
         return url;
@@ -123,7 +127,7 @@ router.get('/qa/build/:number', (req, res, next) => {
 // Get test results from latest qa run
 router.get('/qa/test_report/latest', (req, res, next) => {
     let lastSuccessfulTestReport;
-    getQAJobUrl().then((urlObj) => {
+    getQAJobUrl().then(urlObj => {
         let url = urlObj.qaLastSuccessfulTestReportUrlApi;
         lastSuccessfulTestReport = urlObj.qaLastSuccessfulTestReportUrl
         console.log('Getting ' + url); 
@@ -140,7 +144,7 @@ router.get('/qa/test_report/latest', (req, res, next) => {
 
 // Get test results from specific qa run
 router.get('/qa/test_report/:buildNumber', (req, res, next) => {
-    getQAJobUrl().then((urlObj) => {
+    getQAJobUrl().then(urlObj => {
         let url = urlObj.qaBaseUrl + req.params.buildNumber + '/testReport/' + JSON_API
         console.log('Getting ' + url);
         return url;
